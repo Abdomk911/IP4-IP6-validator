81af1064f9cd2a7978500d43e56b5e8b50f5b1dd
4bdo.mk <wesley sneijder>
2026-09-12 16:03:33 +0100

    final version of the ip validator ligic

--- src/first.ts
+++ src/first.ts
@@ -40,8 +40,36 @@ function mysplit(splitedarr: string[], delimiter: string): string[] {
     part.push(temp);
     return part;
 }
+function isHex(p: string): boolean {
+    for (let c of p) {
+        let isDigit = c >= '0' && c <= '9';
+        let isLow = c >= 'a' && c <= 'f';
+        let isHigh = c >= 'A' && c <= 'F';
+        if (!isDigit && !isLow && !isHigh) return false;
+    }
+    return true;
+}
 let queryIParrP: string[] = mysplit(splitedarr, delimiter);
 function validIPAddress(queryIParrP: string[]): string {
-    if(queryIParrP.length === 4)
+    if (queryIParrP.length === 4) {
+        for (let i = 0; i < queryIParrP.length; i++) {
+            let p = queryIParrP[i]!;
+            if (p.length === 0 || p.length > 3) return "invalid";
+            if (p[0] === '0' && p.length > 1) return "invalid";
+            for (let j = 0; j < p.length; j++) {
+                if (p[j]! < '0' || p[j]! > '9') {
+                    return "invalid";
+                };
+            }
+        }
+         return "IPv4";
+    } else if (queryIParrP.length === 8) {
+        for (let i = 0; i < queryIParrP.length; i++) {
+            let f = queryIParrP[i]!;
+            if (f.length === 0 || f.length > 4) return "invalid";
+            if (!isHex(f)) return "invalid";
+        }
+        return "IPv6";
+    }
     return "invalid";
 }
